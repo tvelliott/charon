a7300e3fc76f4f10e67070a6d779e9615103d366
radioactive <radioactive@debian7>
2018-05-18 12:37:03 -0700

    configuration changes relative to v28 release

---
+++ changes_to_plutosdr_fw_configs_rel_to_v28/buildroot/output/target/root/set_charon_env.sh
@@ -0,0 +1,18 @@
+fw_setenv attr_name compatible
+fw_setenv attr_val ad9364
+fw_setenv maxcpus
+fw_setenv enable_charon 1
+fw_setenv ref_correction_ppm 5.0
+fw_setenv bat_ogm_interval 10000
+fw_setenv max_long_retrans 1
+fw_setenv max_short_retrans 8
+fw_setenv bcast_retrans 1
+fw_setenv max_tcp_segs 2
+fw_setenv usb_batman_if 0
+fw_setenv ack_delay_timeout 30000
+fw_setenv symbol_delay_timeout 144
+fw_setenv max_tcp_share_backoff 1728
+fw_setenv freq_rxtx_hz 915000000 
+fw_setenv sample_freq_hz 1400000 
+fw_setenv rf_bandwidth 250000 
+fw_setenv tx_output_power_minus_dbm 10
